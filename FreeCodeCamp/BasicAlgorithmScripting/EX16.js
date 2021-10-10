6c0a87d07b8be9330a477c2671c089b80755ede2
Leonid Shustov <leonshustov1995@gmail.com>
2021-10-10 18:45:08 +0300

    add EX16

---
+++ FreeCodeCamp/BasicAlgorithmScripting/EX16.js
@@ -0,0 +1,9 @@
+function chunkArrayInGroups(arr, size) {
+    let a = []
+    for (let i = 0; i < arr.length; i += size) {
+        a.push(arr.slice(i, i + size));
+    }
+    return a;
+}
+
+chunkArrayInGroups(["a", "b", "c", "d"], 2);
