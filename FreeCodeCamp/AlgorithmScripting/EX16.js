e3cbb9f900914d3bd5ffaaf2ec00e091d95a9c7c
Leonid Shustov <leonshustov1995@gmail.com>
2021-10-31 20:41:38 +0300

    add EX16

---
+++ FreeCodeCamp/AlgorithmScripting/EX16.js
@@ -0,0 +1,14 @@
+function steamrollArray(arr) {
+    let a = []
+    for (let i = 0; i < arr.length; i++){
+        if(Array.isArray(arr[i])){
+            a.push(...steamrollArray(arr[i]))
+        } else {
+            a.push(arr[i])
+        }
+    }
+
+    return a
+}
+
+console.log(steamrollArray([1, [2], [3, [[4]]]]))
