6131f1829496b39a9d7fc6f4326540338638f3e7
LeonShus <leonshustov1995@gmail.com>
2021-12-03 13:17:54 +0300

    add EX2 & Test For it

--- FreeCodeCamp/AlgoritmsAndDataStruct/Test.test.js
+++ FreeCodeCamp/AlgoritmsAndDataStruct/Test.test.js
@@ -1,10 +1,18 @@
+const {palindrome} = require("./EX01")
+const {convertToRoman} = require("./EX02")
 
-const {palindrome} = require('./EX01')
-
-test('Is Palindrome', () => {
-    expect(palindrome('eye')).toBe(true)
-    expect(palindrome('_eye')).toBe(true)
+test("Is Palindrome", () => {
+    expect(palindrome("eye")).toBe(true)
+    expect(palindrome("_eye")).toBe(true)
     expect(palindrome("race car")).toBe(true)
     expect(palindrome("not a palindrome")).toBe(false)
     expect(palindrome("0_0 (: /-\ :) 0-0")).toBe(true)
+})
+
+
+test("Convert to Roman", () => {
+    expect(convertToRoman(2)).toBe("II")
+    expect(convertToRoman(4)).toBe("IV")
+    expect(convertToRoman(798)).toBe("DCCXCVIII")
+
 })
